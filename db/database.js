e83aece6880a64616694ad04069a90ada2658a6e
thismax <max.g.jacobs@gmail.com>
2017-12-14 21:36:26 -0800

    todo database

--- db/database.js
+++ db/database.js
@@ -1,1 +1,10 @@
 const Sequelize = require('sequelize');
+const url = 'postgres://odewpgko:Gx6SK1I5Z8Sz_idIHyWYBTTGliATAgvR@baasu.db.elephantsql.com:5432/odewpgko';
+const orm = new Sequelize(url);
+
+var Todo = orm.define('Todo', {
+  text: Sequelize.STRING,
+  completed: Sequelize.BOOLEAN
+});
+
+exports.Todo = Todo;
